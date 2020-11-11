2a5472bcc15c49526bf472ad3ba6e11496164d4c
yearn gitbot <73689516+yearnBot@users.noreply.github.com>
2020-11-11 05:34:16 -0800

    v.0.1.0
    v.0.1.0 -- yearn solhint configuration

---
+++ index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+module.exports = {
+  rules: {
+    // Security
+    'compiler-fixed': 'error',
+    'mark-callable-contracts': 'off',
+    // Style
+    indent: ['error', 2]
+  }
+};
